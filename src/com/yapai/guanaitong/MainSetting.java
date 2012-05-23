9c47e247a5648001c9c905ae2b119ba341efd309
Kevin.xie <xyp1984@gmail.com>
2012-05-23 09:23:28 +0800

    fuck.....

--- src/com/yapai/guanaitong/MainSetting.java
+++ src/com/yapai/guanaitong/MainSetting.java
@@ -1,12 +1,27 @@
 package com.yapai.guanaitong;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.util.Log;
 
-public class MainSetting extends Activity {
+public class MainSetting extends PreferenceActivity {
+	final String TAG = "MainSetting";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.main_setting);
+		getWindow().setBackgroundDrawableResource(R.drawable.default_bg);
 	}
+	
+	@Override
+	public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+			Preference preference) {
+		// TODO Auto-generated method stub
+		Log.d(TAG, "onPreferenceTreeClick:"+preference);
+		return super.onPreferenceTreeClick(preferenceScreen, preference);
+	}
+	
 }
